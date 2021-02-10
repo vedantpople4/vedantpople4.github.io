510c6a6a63fff53f0dabd7bbe6898ce723b39c1c
Vedant pople <vedantpople4@gmail.com>
2021-02-10 22:40:03 +0530

    new project directory

--- myportfolio/portfolio/views.py
+++ myportfolio/portfolio/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>This is the home page</h1>')
+    return render(request, 'myportfolio/home.html')
