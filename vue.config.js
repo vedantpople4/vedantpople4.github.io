6f2c6f2cf5ac661c4020aa2a0d4eefac3da9ef82
Vedant Pople <vedantpople4@gmail.com>
2024-02-04 18:56:57 -0700

    Deployed changes

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `@import "@/assets/css/app.scss";`,
+      },
+    },
+  },
+}, 
+{publicPath : process.env.NODE_ENV == 'production'
+    ? '/Portfolio'
+    : ''
+}
+);
